8849aa7e440824294e28084b223fb0b048426565
vikas wankhede <vikas.cool786@gmail.com>
2024-03-31 18:45:52 +0530

    env added

--- app/crime/rail/page.js
+++ app/crime/rail/page.js
@@ -13,7 +13,7 @@ function Rail() {
   useEffect(() => {
     async function fetchRouteData() {
       try {
-        const response = await fetch('http://13.233.101.243:5000/routes/?stat_type=crime&vetted=true&transport_type=rail', {
+        const response = await fetch(process.env.APP_HOST + 'routes/?stat_type=crime&vetted=true&transport_type=rail', {
           method: 'GET',
           headers: {
             Accept: 'application/json',
