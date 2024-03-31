885bfed6de6221ee26915b9bb859f77938e12716
vikas wankhede <vikas.cool786@gmail.com>
2024-03-31 19:01:34 +0530

    env added

--- app/page.js
+++ app/page.js
@@ -17,7 +17,7 @@ export default function Home() {
   });
 
   useEffect(() => {
-    console.log(process.env.NEXT_PUBLIC_APP_HOST);
+    // console.log(process.env.NEXT_PUBLIC_APP_HOST);
     async function fetchData() {
       try {
         const response = await fetch(process.env.NEXT_PUBLIC_APP_HOST + 'dashboard_details?transport_type=rail&published=true', {
@@ -47,7 +47,7 @@ export default function Home() {
       }
     }
 
-    // fetchData();
+    fetchData();
   }, []);
 
   function handleReadMoreToggle(type) {
