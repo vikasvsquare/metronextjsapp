afa3b74666b27f86955e73f5baf3d998328b682d
vikas wankhede <vikas.cool786@gmail.com>
2024-04-04 13:32:45 +0530

    sidebar added

--- app/crime/rail/page.js
+++ app/crime/rail/page.js
@@ -7,6 +7,7 @@ import { usePathname, useSearchParams, useRouter } from 'next/navigation';
 import BarCharts from '@/components/charts/BarCharts';
 import Loader from '@/components/ui/loader';
 import dayjs from 'dayjs';
+import SideBar from '@/components/SideBar';
 
 const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
@@ -16,9 +17,11 @@ let lastQuarter = [];
 
 function Rail() {
   const dateDropdownRef = useRef(null);
+  const router = useRouter();
+  const pathName = usePathname();
+  const searchParams = useSearchParams();
 
   const [vetted, setVetted] = useState(true);
-  const router = useRouter();
   const [routeData, setRouteData] = useState(null);
   const [dateData, setDateData] = useState(null);
   const [ucrData, setUcrData] = useState({});
@@ -29,8 +32,6 @@ function Rail() {
   const [isDatePickerActive, setIsDatePickerActive] = useState(false);
   const [path, setPath] = useState(null);
   const [search, setSearch] = useState(null);
-  const pathName = usePathname();
-  const searchParams = useSearchParams();
 
   let totalSelectedDates = [];
 
@@ -82,13 +83,16 @@ function Rail() {
   useEffect(() => {
     async function fetchDates() {
       try {
-        const response = await fetch(`${process.env.NEXT_PUBLIC_APP_HOST}crime/date_details?published=true&transport_type=rail&vetted=${vetted}`, {
-          method: 'GET',
-          headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json'
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_APP_HOST}crime/date_details?published=true&transport_type=rail&vetted=${vetted}`,
+          {
+            method: 'GET',
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json'
+            }
           }
-        });
+        );
 
         if (!response.ok) {
           throw new Error('Failed to fetch data!');
@@ -128,10 +132,10 @@ function Rail() {
 
           dates.forEach((dateObj) => {
             dateObj.selectedMonths = [];
-    
+
             thisMonth.forEach((date) => {
               const [year] = date.split('-');
-    
+
               if (dateObj.year === year) {
                 dateObj.selectedMonths.push(date);
               }
@@ -346,10 +350,10 @@ function Rail() {
         }
 
         const data = await response.json();
-        const transformedData = data['crime_line_data'].map(item => {
+        const transformedData = data['crime_line_data'].map((item) => {
           return {
             ...item,
-            name: dayjs(item.name).format('MMM YY'),
+            name: dayjs(item.name).format('MMM YY')
           };
         });
 
@@ -396,10 +400,10 @@ function Rail() {
         }
 
         const data = await response.json();
-        const transformedData = data['agency_wide_line_data'].map(item => {
+        const transformedData = data['agency_wide_line_data'].map((item) => {
           return {
             ...item,
-            name: dayjs(item.name).format('MMM YY'),
+            name: dayjs(item.name).format('MMM YY')
           };
         });
 
@@ -545,68 +549,23 @@ function Rail() {
       <DashboardNav />
       <div className="container relative z-10">
         <div className="lg:flex lg:gap-8">
-          <aside className="main-sidebar-sticky relative hidden lg:block lg:basis-3/12 pl-6 pt-6 bg-gradient-to-b from-[#050708] from-[-2.29%] to-[#0089E3] to-[90.57%] min-h-screen rounded-tr-3xl">
-            <div>
-              {/* <div className="relative w-11/12 mr-4 before:inline-block before:w-3.5 before:h-3.5 before:bg-[url('/assets/icon-search.svg')] before:bg-contain before:absolute before:top-1/2 before:-translate-y-1/2 before:left-5">
-                <input
-                  type="search"
-                  name="search"
-                  id="search"
-                  placeholder="Search Route"
-                  className="w-full bg-transparent text-white border border-solid border-white rounded pl-12 py-2 px-4 placeholder:text-center"
-                />
-              </div> */}
-              <Suspense fallback={<Loader />}>
-                <ul className="my-4">
-                  <li>
-                    <button
-                      className={
-                        'text-left font-bold rounded-l-2xl py-3 px-4 lg:px-12 xl:px-20 mr-4 w-full ' +
-                        (!searchData || searchData === 'all' ? ' bg-white text-blue-700' : ' bg-transparent text-white')
-                      }
-                      onClick={() => {
-                        router.push(pathName + '?' + createQueryString('line', 'all'));
-                      }}
-                    >
-                      All Lines
-                    </button>
-                  </li>
-                  {routeData &&
-                    routeData.map((route) => (
-                      <li key={route} style={{ color: 'white' }}>
-                        <button
-                          className={
-                            'text-left font-medium rounded-l-2xl py-3 px-4 lg:px-12 xl:px-20 mr-4 w-full ' +
-                            (searchData && route.toLowerCase().toString().trim() === searchData.toLowerCase().toString().trim()
-                              ? ' bg-white text-blue-700'
-                              : ' bg-transparent text-white')
-                          }
-                          onClick={() => {
-                            router.push(pathName + '?' + createQueryString('line', route));
-                          }}
-                        >
-                          {route}
-                        </button>
-                      </li>
-                    ))}
-                </ul>
-              </Suspense>
-            </div>
-          </aside>
+          <SideBar searchData={searchData} routeData={routeData} createQueryString={createQueryString} />
           <main className="lg:grow lg:basis-9/12 pb-7 lg:pb-8 mt-14">
             <div className="flex flex-wrap items-center justify-between mb-5">
               <h2 className="basis-full sm:basis-6/12 text-2xl lg:text-3xl font-scala-sans font-semibold mt-5 lg:mt-0">All Lines</h2>
               <div className="basis-full sm:basis-6/12 -order-1 sm:order-none flex items-center p-2 gap-2 bg-slate-100 rounded-lg">
                 <button
-                  className={`flex-auto rounded-lg px-4 py-2 flex justify-center items-center ${vetted ? 'bg-gradient-to-r from-[#040E15] from-[5.5%] to-[#17527B] to-[93.69%] text-white' : 'bg-white'
-                    }`}
+                  className={`flex-auto rounded-lg px-4 py-2 flex justify-center items-center ${
+                    vetted ? 'bg-gradient-to-r from-[#040E15] from-[5.5%] to-[#17527B] to-[93.69%] text-white' : 'bg-white'
+                  }`}
                   onClick={() => handleVettedToggle(true)}
                 >
                   <span>Vetted Data</span>
                 </button>
                 <button
-                  className={`flex-auto rounded-lg px-4 py-2 flex justify-center items-center ${!vetted ? 'bg-gradient-to-r from-[#040E15] from-[5.5%] to-[#17527B] to-[93.69%] text-white' : 'bg-white'
-                    }`}
+                  className={`flex-auto rounded-lg px-4 py-2 flex justify-center items-center ${
+                    !vetted ? 'bg-gradient-to-r from-[#040E15] from-[5.5%] to-[#17527B] to-[93.69%] text-white' : 'bg-white'
+                  }`}
                   onClick={() => handleVettedToggle(false)}
                 >
                   <span>Unvetted Data</span>
@@ -649,8 +608,9 @@ function Rail() {
                       </div>
                       <Suspense fallback={<Loader />}>
                         <ul
-                          className={`${isDatePickerActive ? 'flex' : 'hidden'
-                            } flex-col bg-white rounded-lg px-2.5 pb-4 max-h-80 overflow-y-scroll mt-2`}
+                          className={`${
+                            isDatePickerActive ? 'flex' : 'hidden'
+                          } flex-col bg-white rounded-lg px-2.5 pb-4 max-h-80 overflow-y-scroll mt-2`}
                           onClick={(e) => e.stopPropagation()}
                         >
                           {dateData &&
@@ -779,10 +739,11 @@ function Rail() {
                       <ul className="flex justify-between md:justify-start items-center md:gap-6">
                         <li>
                           <button
-                            className={`text-xs lg:text-base first-letter:capitalize ${ucrData.serious_crime.selectedUcr === ''
+                            className={`text-xs lg:text-base first-letter:capitalize ${
+                              ucrData.serious_crime.selectedUcr === ''
                                 ? 'text-black font-bold relative after:absolute after:-bottom-1 after:left-0 after:right-0 after:mx-auto after:w-4/5 after:h-px after:bg-black'
                                 : 'text-slate-500'
-                              }`}
+                            }`}
                             onClick={() => handleCrimeCategoryChange('serious_crime', '')}
                           >
                             All
@@ -818,9 +779,7 @@ function Rail() {
               <div className="grid grid-cols-1 lg:grid-cols-2 lg:gap-x-5">
                 <div className="bg-white py-4 px-4 text-sm lg:text-base text-slate-400 rounded-lg mt-6">
                   {/* <h6 className="inline-block text-xxs font-bold border-b border-solid border-sky-400 mb-4">UNDER PERSON CRIME</h6> */}
-                  <Suspense fallback={<Loader />}>
-                    {barData.serious_crime && <BarCharts chartData={barData.serious_crime} />}
-                  </Suspense>
+                  <Suspense fallback={<Loader />}>{barData.serious_crime && <BarCharts chartData={barData.serious_crime} />}</Suspense>
                 </div>
                 <div className="bg-white py-4 px-4 text-slate-400 rounded-lg mt-6 w-full" style={{ fontSize: 11, padding: '10px 0' }}>
                   <Suspense fallback={<Loader />}>
@@ -845,10 +804,11 @@ function Rail() {
                       <ul className="flex justify-between md:justify-start items-center md:gap-6">
                         <li>
                           <button
-                            className={`text-xs lg:text-base first-letter:capitalize ${ucrData.general_crime.selectedUcr === ''
+                            className={`text-xs lg:text-base first-letter:capitalize ${
+                              ucrData.general_crime.selectedUcr === ''
                                 ? 'text-black font-bold relative after:absolute after:-bottom-1 after:left-0 after:right-0 after:mx-auto after:w-4/5 after:h-px after:bg-black'
                                 : 'text-slate-500'
-                              }`}
+                            }`}
                             onClick={() => handleCrimeCategoryChange('general_crime', '')}
                           >
                             All
@@ -884,9 +844,7 @@ function Rail() {
               <div className="grid grid-cols-1 lg:grid-cols-2 lg:gap-x-5">
                 <div className="bg-white py-4 px-4 text-sm lg:text-base text-slate-400 rounded-lg mt-6">
                   {/* <h6 className="inline-block text-xxs font-bold border-b border-solid border-sky-400 mb-4">UNDER PERSON CRIME</h6> */}
-                  <Suspense fallback={<Loader />}>
-                    {barData.general_crime && <BarCharts chartData={barData.general_crime} />}
-                  </Suspense>
+                  <Suspense fallback={<Loader />}>{barData.general_crime && <BarCharts chartData={barData.general_crime} />}</Suspense>
                 </div>
                 <div className="bg-white py-4 px-4 text-slate-400 rounded-lg mt-6 w-full" style={{ fontSize: 11, padding: '10px 0' }}>
                   <Suspense fallback={<Loader />}>
@@ -911,10 +869,11 @@ function Rail() {
                       <ul className="flex justify-between md:justify-start items-center md:gap-6">
                         <li>
                           <button
-                            className={`text-xs lg:text-base first-letter:capitalize ${ucrData.agency_wide.selectedUcr === ''
+                            className={`text-xs lg:text-base first-letter:capitalize ${
+                              ucrData.agency_wide.selectedUcr === ''
                                 ? 'text-black font-bold relative after:absolute after:-bottom-1 after:left-0 after:right-0 after:mx-auto after:w-4/5 after:h-px after:bg-black'
                                 : 'text-slate-500'
-                              }`}
+                            }`}
                             onClick={() => handleCrimeCategoryChange('agency_wide', '')}
                           >
                             All
@@ -950,9 +909,7 @@ function Rail() {
               <div className="grid grid-cols-1 lg:grid-cols-2 lg:gap-x-5">
                 <div className="bg-white py-4 px-4 text-sm lg:text-base text-slate-400 rounded-lg mt-6">
                   {/* <h6 className="inline-block text-xxs font-bold border-b border-solid border-sky-400 mb-4">UNDER PERSON CRIME</h6> */}
-                  <Suspense fallback={<Loader />}>
-                    {barData.agency_wide  && <BarCharts chartData={barData.agency_wide } />}
-                  </Suspense>
+                  <Suspense fallback={<Loader />}>{barData.agency_wide && <BarCharts chartData={barData.agency_wide} />}</Suspense>
                 </div>
                 <div className="bg-white py-4 px-4 text-slate-400 rounded-lg mt-6 w-full" style={{ fontSize: 11, padding: '10px 0' }}>
                   <Suspense fallback={<Loader />}>
---
+++ components/SideBar.js
@@ -0,0 +1,62 @@
+import React, { Suspense } from 'react';
+import { usePathname, useRouter } from 'next/navigation';
+import Loader from '@/components/ui/loader';
+
+function SideBar({ searchData, routeData, createQueryString }) {
+  const router = useRouter();
+  const pathName = usePathname();
+  return (
+    <>
+      <aside className="main-sidebar-sticky relative hidden lg:block lg:basis-3/12 pl-6 pt-6 bg-gradient-to-b from-[#050708] from-[-2.29%] to-[#0089E3] to-[90.57%] min-h-screen rounded-tr-3xl">
+        <div>
+          {/* <div className="relative w-11/12 mr-4 before:inline-block before:w-3.5 before:h-3.5 before:bg-[url('/assets/icon-search.svg')] before:bg-contain before:absolute before:top-1/2 before:-translate-y-1/2 before:left-5">
+            <input
+              type="search"
+              name="search"
+              id="search"
+              placeholder="Search Route"
+              className="w-full bg-transparent text-white border border-solid border-white rounded pl-12 py-2 px-4 placeholder:text-center"
+            />
+          </div> */}
+          <Suspense fallback={<Loader />}>
+            <ul className="my-4">
+              <li>
+                <button
+                  className={
+                    'text-left font-bold rounded-l-2xl py-3 px-4 lg:px-12 xl:px-20 mr-4 w-full ' +
+                    (!searchData || searchData === 'all' ? ' bg-white text-blue-700' : ' bg-transparent text-white')
+                  }
+                  onClick={() => {
+                    router.push(pathName + '?' + createQueryString('line', 'all'));
+                  }}
+                >
+                  All Lines
+                </button>
+              </li>
+              {routeData &&
+                routeData.map((route) => (
+                  <li key={route} style={{ color: 'white' }}>
+                    <button
+                      className={
+                        'text-left font-medium rounded-l-2xl py-3 px-4 lg:px-12 xl:px-20 mr-4 w-full ' +
+                        (searchData && route.toLowerCase().toString().trim() === searchData.toLowerCase().toString().trim()
+                          ? ' bg-white text-blue-700'
+                          : ' bg-transparent text-white')
+                      }
+                      onClick={() => {
+                        router.push(pathName + '?' + createQueryString('line', route));
+                      }}
+                    >
+                      {route}
+                    </button>
+                  </li>
+                ))}
+            </ul>
+          </Suspense>
+        </div>
+      </aside>
+    </>
+  );
+}
+
+export default SideBar;
