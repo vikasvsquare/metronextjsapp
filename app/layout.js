17b9e1c1a8002b7f6518bd7076ccb625c0f60652
Maninder Singh <maninder.singh@vsquaresystem.com>
2024-04-04 22:58:33 +0530

    style: add and change fonts

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,5 @@
 import Context from '@/store/context';
-import { Inter } from 'next/font/google';
 import './globals.css';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Create Next App',
@@ -12,7 +9,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <head>
+        <link rel="stylesheet" href="https://use.typekit.net/goe1fni.css" />
+      </head>
+      <body>
         <div className="bg-slate-100">
           <div className="max-w-[1440px] mx-auto bg-white">
             <Context>{children}</Context>
