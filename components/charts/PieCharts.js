b8696bf1bda85ac0d31bb8a3f4544e246e9d4b11
vikas wankhede <vikas.cool786@gmail.com>
2024-04-08 23:19:19 +0530

    piechart added

--- components/charts/PieCharts.js
+++ components/charts/PieCharts.js
@@ -25,15 +25,15 @@ const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, per
 function PieCharts() {
 
   return (
-    <ResponsiveContainer width="100%" height="100%">
-        <PieChart width={400} height={400}>
+    <ResponsiveContainer width="100%" height={350}>
+        <PieChart width={500} height={500}>
           <Pie
             data={data}
             cx="50%"
             cy="50%"
             labelLine={false}
             label={renderCustomizedLabel}
-            outerRadius={80}
+            // outerRadius={80}
             fill="#8884d8"
             dataKey="value"
           >
