fe81cbefc5ad5b4b52eb53887911c3b1f42c1aeb
vikas wankhede <vikas.cool786@gmail.com>
2024-08-19 17:28:23 +0530

    security added

--- app/layout.js
+++ app/layout.js
@@ -19,7 +19,6 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
-      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
       <body>
       <AuthWrapper>
       {children}
